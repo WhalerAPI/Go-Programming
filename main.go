7124341b4e9deaad64a94094f82ee1cfe89bb758
xWhaler <camelbyte@proton.me>
2025-06-11 08:15:51 -0400

    fixed treesort, Appendvalues, Add -- /tree/tree.go

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
+	"golang/tree"
 	"golang/utils"
 	"strings"
-	"golang/tree"
 )
 
 
@@ -22,14 +22,23 @@ func main() {
 
 	a := utils.ReverseArrayCopy(names)
 	fmt.Println(a)
-
-	s := strings.Join(names, email)
-	fmt.Println(s)
-
 	v := []int{1,2,3,4,54,4,5,6,67,78}
-	fmt.Println(len(v)-1-len(v))
 	flip := utils.Flip(v)
 	fmt.Println(flip)
 
+	// Treesort the slice of values
 	fmt.Println(tree.TreeSort(v))
+
+
+	// initialize a tree using our definition from tree/tree.go
+	node := &tree.Tree{}
+	// Tree is currently empty.
+	// Functions of *Tree include [add, appendValues]
+	fmt.Println(node)
+	node.Add(4)
+	node.AppendValues(v)
+	fmt.Println(node)
+	
+
+
 }
