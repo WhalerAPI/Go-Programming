a5a21e3be41d4917a8599026d12fd2a23fb23bd1
Keith Thomson <camelbyte@proton.me>
2025-07-01 21:30:17 -0400

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,13 @@ func main() {
 
 	s := functions.CollectStuff()
 	fmt.Println("\nHere is a collection of stuff: \n\n", s)
+
+
 	
+	// use treesort 
+	// have an array var a []int{1,2,3,4,23,4,5,}
+	// fmt.Println(TreeSort(a))
+	// result {1,2,3,4,4,5,23}
 
 
 }
